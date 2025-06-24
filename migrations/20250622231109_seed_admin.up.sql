42bb5f200e4fb09a0b904a29f25d61100a807729
Rafael Gomides <mrrafagomides@gmail.com>
2025-06-23 23:28:15 -0300

    ajustes infra

--- migrations/20250622231109_seed_admin.up.sql
+++ migrations/20250622231109_seed_admin.up.sql
@@ -1,11 +1,11 @@
 /* bcrypt hash da senha "admin123"
   gerado com: echo -n 'admin123' | bcrypt
-  $2a$10$0uv5XvSLV3zOcnpMj8/RMeM8E81lxZiRvXj9l5fNfuD4.DkJrOe2O
+  $2a$10$iu30miJvgCprRhh2tzbQzO2lF2l7OD4ZOrv3oj7LNZ3K2Qt9nwmMq
 */
 
 INSERT INTO users (id, email, password_hash, full_name)
 VALUES ('01HX0000000000000000000010', 'admin@rgps.tech',
-        '$2a$10$0uv5XvSLV3zOcnpMj8/RMeM8E81lxZiRvXj9l5fNfuD4.DkJrOe2O',
+        '$2a$10$iu30miJvgCprRhh2tzbQzO2lF2l7OD4ZOrv3oj7LNZ3K2Qt9nwmMq',
         'Admin User');
 
 INSERT INTO user_roles (user_id, role_id)
